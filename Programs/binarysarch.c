0a87805213e528b15e9186b441bce8603615536b
Kyle Lewis <kylelew003@gmail.com>
2022-09-30 19:38:54 +0530

    changes

--- Programs/binarysarch.c
+++ Programs/binarysarch.c
@@ -1,7 +1,66 @@
 #include<stdio.h>
 int main()
 {
-    int a[10];
-    int n,i,j,temp,key;
+    int n,i,j,temp;
+    printf("Enter the number of elements : \n");
+    scanf("%d",&n);
 
+    int a[n];
+
+    printf("Enter array elements : \n");
+    for(i = 0 ; i<n ; i++)
+    {
+        scanf("%d",a[i]);
+    }
+    printf("Array before sorting : \n");
+    for(i = 0 ; i<n ; i++)
+    {
+        printf("%d ",a[i]);
+    }
+
+    for(i = 0 ; i<n-1 ; i++)
+    {
+        for(j = 0 ; j<n-i-1 ; j++)
+        {
+            if(a[j]>a[j+1])
+            {
+                temp = a[j];
+                a[j] = a[j+1];
+                a[j+1] = temp;
+            }
+        }
+    }
+    printf("Array before sorting : \n");
+    for(i = 0 ; i<n ; i++)
+    {
+        printf("%d ",a[i]);
+    }
+
+    int high , low , mid , x;
+
+    printf("Enter the array element to be searched in the array : \n");
+    scanf("%d",&x);
+
+    low = 0;
+    high = n-1;
+
+    while (low<=high)
+    {
+        mid = (low + high)/2;
+        if(x == a[mid])
+        {
+            printf("The element %d is found at location %d\n",x,mid+1);
+            return 0;
+        }
+        else if(x<a[mid])
+        {
+            high = mid-1;
+        }
+        else
+        {
+            low = mid + 1;
+        }
+    }
+    printf("The element %d is not found in the array \n",x);
+    
 }
